da8aa708d3e04f0fe7892149227540ca930211e4
Onur İrken <irkenonur@gmail.com>
2021-06-22 18:30:11 +0300

    - Add react-router and route renderer

---
+++ src/components/render-routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import RouteWithSubRoutes from "./route-with-sub-routes";
+import { Route, Switch } from "react-router-dom";
+import { EmptyPage } from "components";
+
+const RenderRoutes = ({ routes }) => {
+  return (
+    <Switch>
+      {routes.map((route, i) => {
+        return <RouteWithSubRoutes key={route.key} {...route} />;
+      })}
+      <Route
+        component={() => (
+          <EmptyPage
+            title="NOT FOUND"
+            message="A page matching the url you entered was not found
+"
+          />
+        )}
+      />
+    </Switch>
+  );
+};
+
+export default RenderRoutes;
