147cec448c29cd8882715258aa08c0f50abb4e09
Onur İrken <irkenonur@gmail.com>
2021-06-22 18:50:14 +0300

    - Prettier

--- src/components/empty-page.js
+++ src/components/empty-page.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const EmptyPage = () => {
-  return <div>Empty Page</div>
-}
+  return <div>Empty Page</div>;
+};
 
-export default EmptyPage
+export default EmptyPage;
